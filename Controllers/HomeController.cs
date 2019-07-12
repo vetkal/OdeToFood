1b6e1bbc27b78591cd35710c57ca39f4e83b3b72
Kallåk <Vetle.Kallak@dnvgl.com>
2019-07-12 15:38:05 +0200

    tried to make db, not sucessfull

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,16 +9,20 @@ namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
+        OdeToFoodDb _db = new OdeToFoodDb();
+
         public ActionResult Index()
         {
-            var controller = RouteData.Values["controller"];
-            var action = RouteData.Values["action"];
-            var id = RouteData.Values["id"];
+            //var controller = RouteData.Values["controller"];
+            //var action = RouteData.Values["action"];
+            //var id = RouteData.Values["id"];
 
-            var message = String.Format("{0}::{1} {2}", controller, action, id);
-            ViewBag.message = message;
+            //var message = String.Format("{0}::{1} {2}", controller, action, id);
 
-            return View();
+            //ViewBag.message = message;
+            var model = _db.Restaurants.ToList();
+                       
+            return View(model);
         }
 
         public ActionResult About()
@@ -36,6 +40,15 @@ namespace OdeToFood.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ Models/OdeToFoodDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// using System.Data.Entity;
+
+
+namespace OdeToFood.Models
+{
+    public class OdeToFoodDb : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<RestaurantReview> Reviews { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=Blogging;Integrated Security=True");
+        }
+    }
+}
