082680fa68ae2014b445a4e687636bee55a2639a
Kallåk <Vetle.Kallak@dnvgl.com>
2019-07-12 10:59:12 +0200

    Finish Razor Views module, 1. commit

---
+++ Controllers/CuisineController.cs
@@ -0,0 +1,22 @@
+﻿using OdeToFood.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class CuisineController : Controller
+    {
+        // GET: Cuisine
+        [HttpGet]
+        [Log]
+        public ActionResult Search(string name)
+        {
+            var message = Server.HtmlEncode(name);
+
+            return Content(message);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+
+            var message = String.Format("{0}::{1} {2}", controller, action, id);
+            ViewBag.message = message;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var model = new AboutModel();
+            model.Name = "Vetle";
+            model.Location = "Baerum, NOR";
+
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
